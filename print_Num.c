0458038f40878ebce59c2346c621e43ec2af2275
SamarMorsy <samar.magdy.ahmed@gmail.com>
2023-09-19 06:52:41 -0700

    add print num

---
+++ print_Num.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_int - print numbers
+ * @n: number
+ * @counter: number of digits
+ * Return: counter
+*/
+int print_int(int n, int counter)
+{
+	if (n < 0)
+	{
+		_i  putchar('-');
+		n = -n;
+		counter++;
+	}
+	if (n / 10 != 0)
+		counter =  print_int(n / 10, ++counter);
+	putchar((n % 10) + '0');
+	return (counter);
+	}
+
